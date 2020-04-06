0997472da52aa24a5c231b219364f9564b085c4b
Tika Tsiklauri <tika1114@gmail.com>
2020-04-06 16:52:32 -0400

    backendauth part

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,28 @@
+class Api::SessionsController < ApplicationController 
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+
+        if @user
+            login!(@user)
+            render '/api/users/show'
+        else
+            render json: ["Invalid Username and/or Password"], status: 401
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render "api/users/show"
+        else
+            render json: ["Not Signed In"], status:404
+        end
+    end
+
+
+end
