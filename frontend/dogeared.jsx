890a8a0328e2dfce3d293e100d933abb8878f71a
Tika Tsiklauri <tika1114@gmail.com>
2020-04-08 09:42:25 -0400

    more auth

--- frontend/dogeared.jsx
+++ frontend/dogeared.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from "react-dom";
 
 document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Dogeared!</h1>, root)
 });
