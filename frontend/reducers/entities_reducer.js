5a694f070c2ddf0b6115079490046736eed48a55
Tika Tsiklauri <tika1114@gmail.com>
2020-04-15 00:32:46 -0400

    modified entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,18 +1,12 @@
 import {combineReducers} from 'redux';
 import usersReducer from './users_reducer';
-<<<<<<< HEAD
 import formTypeReducer from './form_type_reducer';
-
-const entitiesReducer = combineReducers({
-    users: usersReducer,
-    formType: formTypeReducer
-=======
 import booksReducer from './books_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
+    formType: formTypeReducer
     books: booksReducer
->>>>>>> Books
 });
 
 export default entitiesReducer;
