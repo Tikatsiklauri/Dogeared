98165252ffc6b727ca89388c06ec48b117b64b16
Tika Tsiklauri <tika1114@gmail.com>
2020-06-04 16:50:48 -0400

    create in book_on_shelf

--- app/controllers/api/book_on_shelf_controller.rb
+++ app/controllers/api/book_on_shelf_controller.rb
@@ -11,7 +11,33 @@ class Api::BookOnShelfController < ApplicationController
     end
 
     def create
+        @new_shelf = BookOnShelf.new(book_on_shelf_params)
+        @curretn_user = Shelf.find_by(id: :book_on_shelf_params[:shelf_id].user_id)
+        @shelf_name = Shelf.find_by(:id @new_shelf.shelf_id).name
 
+        if @new_shelf.save
+            if @shelf_name == "Read" ||
+                @shelf_name == "Want to Read" || 
+                @shelf_name == "Currently Reading"
+
+                @newShelves = Book.find_by(id: :book_on_shelf_params[:book_id].shelves.where(user_id: @curretn_user))
+                @newShelves.each do |shelf|
+                    if shelf.name == "Read" ||
+                        shelf.name == "Want to Read" ||
+                        shelf.name == "Currently Reading" 
+
+                        @book_on_shelf_undo = Shelf.find_by(:id shelf.id).book_on_shelf.where(:book_id :book_on_shelf_params[:book_id])[0]
+                        if @new_shelf.id != @book_on_shelf_undo.id
+                            @book_on_shelf_undo.destroy
+                        end
+                    end
+                end
+            end
+            if @shelves
+                @shelves = Book.find_by(:id book_on_shelf_params[:book_id]).shelves.where(user_id: @curretn_user)
+            end
+            render :show
+        end
     end
     
     
@@ -21,7 +47,7 @@ class Api::BookOnShelfController < ApplicationController
     
     private
 
-    def Book_On_Shelf_params
+    def book_on_shelf_params
         params.require(:book_on_shelf).permit(:book_id, :shelf_id)
     end  
     
