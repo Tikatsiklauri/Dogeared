436be9031915b0247d3417fa4650cf6615e9d69f
Tika Tsiklauri <tika1114@gmail.com>
2020-06-10 20:09:03 -0400

    assosiation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
     attr_reader :password
 
+    has_many :shelves,
+    foreign_key: :user_id,
+    class_name: :Shelf
+
      def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         return nil unless user && user.is_password?(password)
