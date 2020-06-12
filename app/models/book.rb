f640b52db8c7c32a558981668b5a401f0f8448dc
Tika Tsiklauri <tika1114@gmail.com>
2020-06-12 15:28:10 -0400

    association fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,5 @@ class Book < ApplicationRecord
     validates :title, :author, presence: true, uniqueness:true
     validates :date, :overview, :genre, presence: true
 
-    has_many :books_on_shelves,
-    class_name: :Book_On_Shelf,
-    foreign_key: :book_id
-
-    has_many :shelves,
-    through: :books_on_shelves,
-    source: :shelf
+    belongs_to :shelf
 end
