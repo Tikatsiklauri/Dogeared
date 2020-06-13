733f21195420653551ed4b2d903ad918395c1731
Tika Tsiklauri <tika1114@gmail.com>
2020-06-13 15:30:13 -0400

    reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,14 +3,13 @@ import usersReducer from './users_reducer';
 import formTypeReducer from './form_type_reducer';
 import booksReducer from './books_reducer';
 import shelf_reducer from './shelf_reducer';
-import books_on_shelves_reducer from './books_on_shelves_reducer';
+
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     formType: formTypeReducer,
     books: booksReducer,
-    shelves: shelf_reducer,
-    books_on_shelves: books_on_shelves_reducer
+    shelves: shelf_reducer, 
 });
 
 export default entitiesReducer;
