6416c165c0f13115bd020417b2706f4224c866cc
Tika Tsiklauri <tika1114@gmail.com>
2020-06-24 19:13:34 -0400

    styling stars

--- frontend/components/reviews/create_review.jsx
+++ frontend/components/reviews/create_review.jsx
@@ -30,23 +30,35 @@ class CreateReview extends React.Component {
     }
 
     render() {
-
+       
         return (
             <div id="reviewForm">
                 <form onSubmit={this.handleSubmit}>
                     <div id="ratingDiv">
-                        <label id="myRating">My rating:</label>
-                        <div className="rating">
-                            <input type="radio" id="star5" name="rate" value="5" onChange={this.update('rating')} />
+        <label id="myRating">{this.props.userName}, Start your review of {this.props.bookTitle}</label>
+                        {/* <div className="rating">
+                            <input type="radio" id="star5" name="rate" value="1" onChange={this.update('rating')} />
                             <label htmlFor="star5" title="text"></label>
-                            <input type="radio" id="star4" name="rate" value="4" onChange={this.update('rating')} />
+                            <input type="radio" id="star4" name="rate" value="2" onChange={this.update('rating')} />
                             <label htmlFor="star4" title="text"></label>
                             <input type="radio" id="star3" name="rate" value="3" onChange={this.update('rating')} />
                             <label htmlFor="star3" title="text"></label>
-                            <input type="radio" id="star2" name="rate" value="2" onChange={this.update('rating')} />
+                            <input type="radio" id="star2" name="rate" value="4" onChange={this.update('rating')} />
                             <label htmlFor="star2" title="text"></label>
-                            <input type="radio" id="star1" name="rate" value="1" onChange={this.update('rating')} />
+                            <input type="radio" id="star1" name="rate" value="5" onChange={this.update('rating')} />
                             <label htmlFor="star1" title="text"></label>
+                        </div> */}
+                        <div class="rate">
+                            <input type="radio" id="star5" name="rate" value="5" onChange={this.update('rating')} />
+                            <label for="star5" title="text">5 stars</label>
+                            <input type="radio" id="star4" name="rate" value="4" onChange={this.update('rating')}/>
+                            <label for="star4" title="text">4 stars</label>
+                            <input type="radio" id="star3" name="rate" value="3" onChange={this.update('rating')}/>
+                            <label for="star3" title="text">3 stars</label>
+                            <input type="radio" id="star2" name="rate" value="2" onChange={this.update('rating')}/>
+                            <label for="star2" title="text">2 stars</label>
+                            <input type="radio" id="star1" name="rate" value="1" onChange={this.update('rating')}/>
+                            <label for="star1" title="text">1 star</label>
                         </div>
                     </div>
                     <br />
@@ -64,7 +76,7 @@ class CreateReview extends React.Component {
                         />
                     </div>
 
-                    <input className="submit" type="submit" value={this.props.type.type} />
+                    <input className="submit" type="submit" value={this.props.type} />
                 </form>
             </div>
         );
